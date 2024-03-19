b50638b6868ea102fdf25a0f757aecd2118bc415
Kseniia Riabova <kseniaistratova@gmail.com>
2024-03-19 12:12:19 +0100

    Initial commit

---
+++ dino-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Home from './pages/Home.jsx';
+
+import { useStatemp } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+export default App;
---
+++ dino-app/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function home() {
+  return (
+    <div>
+      <div className="w-full justify-center align-center h-full ">
+        <div className="flex flex-row justify-between align-center">
+          <button>All Dinosaurs</button>
+          <button>Interesting Facts about Dinosaurs</button>
+          <button>Quiz</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default home;
