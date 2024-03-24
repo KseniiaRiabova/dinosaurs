74ebd05c7f948370392e739b7231305573aca96c
Kseniia Riabova <kseniaistratova@gmail.com>
2024-03-24 22:19:06 +0100

    Add Hook useFetch, update routing, add redirect to buttons
    Updated Home,Map,Quiz, Dinos pages. Created and implemented useFetch.

---
+++ dino-app/src/components/useFetch.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function useFetch(url) {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+
+        const response = await axios.get(url, {
+          timeout: 15000,
+          responseType: 'json',
+        });
+
+        const responseData = response.data;
+        setData(responseData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, error, isLoading };
+}
+
+export default useFetch;
